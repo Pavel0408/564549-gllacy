979d77ae4757a87bfb2a70c7461f62e51bfff59b
Pavel0408 <pavel0408@gmail.com>
2018-09-05 21:21:07 +0300

    Оживил форму и добавил интерактивную карту

---
+++ js/script.js
@@ -0,0 +1,60 @@
+var FormLink = document.querySelector(".btn-form");
+var feedbackForm = document.querySelector(".feedback-form");
+var CloseButton = document.querySelector(".close-button")
+var userName = feedbackForm.querySelector("[name=name]");
+var commentsForm = document.querySelector(".comments-form");
+var emailFeedback = feedbackForm.querySelector("[name=email-feedback]");
+var message = feedbackForm.querySelector("[name=message-area]");
+
+
+var storage = "";
+var storsgeSupport = true;
+try {
+  storage = localStorage.getItem("userName");
+} catch (err) {
+  storage = false;
+  storageSupport = false;
+}
+
+
+FormLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackForm.classList.add("show");
+  if (storage) {
+    userName.value = storage;
+    emailFeedback.value = localStorage.emailFeedback;
+    message.focus;
+
+  } else {
+    userName.focus();
+  }
+
+});
+
+CloseButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackForm.classList.remove("show");
+  feedbackForm.classList.remove("modal-error");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (feedbackForm.classList.contains("show")) {
+      evt.preventDefault();
+      feedbackForm.classList.remove("show");
+      feedbackForm.classList.remove("modal-error");
+    }
+  }
+});
+
+commentsForm.addEventListener("submit", function (evt) {
+  if (!userName.value || !emailFeedback.value || !message.value) {
+    evt.preventDefault();
+    feedbackForm.classList.remove("modal-error");
+    feedbackForm.classList.add("modal-error");
+  } else if (storageSupport) {
+    localStorage.setItem("userName", userName.value);
+    localStorage.setItem("emailFeedback", emailFeedback.value);
+
+  }
+})
